e7fc81608a7b1584ef5785de1b797f97307a9d65
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2025-04-20 19:09:49 +0300

    Initial commit

---
+++ Zhytnetskyi-SwiftUI-App/Zhytnetskyi_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zhytnetskyi_SwiftUI_AppApp.swift
+//  Zhytnetskyi-SwiftUI-App
+//
+//  Created by Oleksiy Zhytnetsky on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Zhytnetskyi_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
